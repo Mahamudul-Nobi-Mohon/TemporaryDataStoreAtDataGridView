d95384d373e2d766b40a29d8269dfb3455661bd0
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2017-05-14 11:03:14 +0600

    new project for temporary data store add datagrid

---
+++ TemporaryDataInsertAtDataGridView/MyForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TemporaryDataInsertAtDataGridView
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonSaveInDatabase = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(120, 28);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(302, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(23, 31);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(35, 13);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Name";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(120, 113);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(396, 188);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(120, 69);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(302, 20);
+            this.textBoxPrice.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(238, 113);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(347, 113);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonSaveInDatabase
+            // 
+            this.buttonSaveInDatabase.Location = new System.Drawing.Point(158, 360);
+            this.buttonSaveInDatabase.Name = "buttonSaveInDatabase";
+            this.buttonSaveInDatabase.Size = new System.Drawing.Size(183, 64);
+            this.buttonSaveInDatabase.TabIndex = 2;
+            this.buttonSaveInDatabase.Text = "Save in Database";
+            this.buttonSaveInDatabase.UseVisualStyleBackColor = true;
+            this.buttonSaveInDatabase.Click += new System.EventHandler(this.buttonSaveInDatabase_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 447);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSaveInDatabase);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxName);
+            //this.Name = "MyForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonSaveInDatabase;
+    }
+}
+
---
+++ TemporaryDataInsertAtDataGridView/MyForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemporaryDataInsertAtDataGridView
+{
+    public partial class MyForm : Form
+    {
+        DataTable dt = new DataTable();
+        int indexRows;
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int isEdited = 0;
+
+            if (textBoxPrice.Text == "")
+            {
+                textBoxPrice.Text = "0";
+                
+            }
+
+            if (dataGridView1.Rows.Count > 0)
+            {
+
+
+                String searchValue = textBoxName.Text;
+                int rowIndex = -1;
+                if(isEdited == 0)
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (row.Cells["Name"].Value != null) // Need to check for null if new row is exposed
+                        {
+                            rowIndex = -1;
+                            if (row.Cells["Name"].Value.ToString().Equals(searchValue))
+                            {
+                                rowIndex = row.Index;
+                                // break;
+                                DataGridViewRow newDataRow = dataGridView1.Rows[rowIndex];
+                                double sum = Convert.ToDouble(newDataRow.Cells[1].Value) + Convert.ToDouble(textBoxPrice.Text);
+                                newDataRow.Cells[0].Value = textBoxName.Text;
+                                newDataRow.Cells[1].Value = sum;
+                                isEdited = 1;
+                                break;
+                            }
+                           
+                        }
+                    }
+                }
+       
+            }
+            if (isEdited != 1)
+            {
+                dt.Rows.Add(textBoxName.Text, textBoxPrice.Text);
+                this.dataGridView1.DataSource = dt;
+                clearAll();
+            }
+
+            //else
+            // {
+            //     dt.Rows.Add(textBoxName.Text, textBoxPrice.Text);
+            //     this.dataGridView1.DataSource = dt;
+            //     clearAll();
+            // }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Name", typeof(string)),
+                new DataColumn("Price", typeof(double))});
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxName.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBoxPrice.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 1)
+            {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(rowIndex);
+            clearAll();
+            }
+            else
+            {
+                MessageBox.Show("Please select a data. then click Remove button.");
+            }
+          }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (textBoxPrice.Text == "")
+            {
+                textBoxPrice.Text = "0";
+            }
+            if (dataGridView1.Rows.Count > 1)
+            {
+                indexRows = dataGridView1.CurrentCell.RowIndex;
+                DataGridViewRow newDataRow = dataGridView1.Rows[indexRows];
+                newDataRow.Cells[0].Value = textBoxName.Text;
+                newDataRow.Cells[1].Value = textBoxPrice.Text;
+                clearAll();
+            }
+            else
+            {
+                MessageBox.Show("Please select a data. then click update button.");
+            }
+
+        }
+
+        private void buttonSaveInDatabase_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=MOHON-PC;Initial Catalog=Sample_30-11-2016;Integrated Security=True");
+            con.Open();
+            int n = 0;
+
+            foreach (DataRow item in dt.Rows)
+            {
+               
+                string name = dataGridView1.Rows[n].Cells[0].Value.ToString();
+               double price =Convert.ToDouble( dataGridView1.Rows[n].Cells[1].Value);
+
+                string query = "INSERT INTO Employee2 (Name,Price) VALUES ('"+ name + "', "+price+")";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.ExecuteNonQuery();
+
+                n++;
+
+
+                
+            }
+            con.Close();
+
+            MessageBox.Show("Insert Successfully");
+            clearAll();
+        }
+
+        private void clearAll()
+        {
+            textBoxName.Text = string.Empty;
+            textBoxPrice.Text = string.Empty;
+        }
+        
+    }
+}
